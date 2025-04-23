d21b8050ed3969e3a5a82d1ed2ba44235af18007
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-04-23 13:52:58 -0400

    Se establece la configuración de la colección blog

---
+++ src/content/config.ts
@@ -0,0 +1,30 @@
+import { defineCollection, z } from "astro:content";
+
+const blogCollection = defineCollection({
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		date: z.date(),
+		description: z.string(),
+		image: z.string(),
+
+		// Relación
+		author: z.string(),
+
+		// Relación
+		tags: z.array(z.string()),
+	}),
+});
+
+export const collections = {
+    blog: blogCollection,
+    // la carpeta de post tiene que llamarse igual que la colección, en este caso blog
+    // si no se llama igual, no se va a poder acceder a los posts desde la colección
+    // se pueden crear más colecciones, pero tienen que tener su propia carpeta
+    // y su propio esquema, por ejemplo:
+    // portfolio: defineCollection({
+    //     type: "content",
+    //     schema: z.object({
+    //         title: z.string(),
+    //         date: z.date(), y así
+};
