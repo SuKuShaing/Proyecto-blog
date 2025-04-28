8fc78d1d60265c9bd5368aa83dbdf0c8e82be368
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-04-28 17:03:00 -0400

    se añade authorCollection para poder hacer la relación entre autores

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,30 +1,44 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const blogCollection = defineCollection({
 	type: "content",
-	schema: ({ image }) => z.object({
-		title: z.string(),
-		date: z.date(),
-		description: z.string(),
-		image: image(),
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			date: z.date(),
+			description: z.string(),
+			image: image(),
 
-		// Relación
-		author: z.string(),
+			// Relación
+			// author: z.string(),
+			author: reference('author'),
 
-		// Relación
-		tags: z.array(z.string()),
-	}),
+			// Relación
+			tags: z.array(z.string()),
+		}),
+});
+
+const authorCollection = defineCollection({
+	type: "data",
+	schema: ({ image }) =>
+		z.object({
+			name: z.string(),
+			avatar: image(),
+		}),
 });
 
 export const collections = {
-    blog: blogCollection,
-    // la carpeta de post tiene que llamarse igual que la colección, en este caso blog
-    // si no se llama igual, no se va a poder acceder a los posts desde la colección
-    // se pueden crear más colecciones, pero tienen que tener su propia carpeta
-    // y su propio esquema, por ejemplo:
-    // portfolio: defineCollection({
-    //     type: "content",
-    //     schema: z.object({
-    //         title: z.string(),
-    //         date: z.date(), y así
+	blog: blogCollection,
+
+	// la carpeta de post tiene que llamarse igual que la colección, en este caso blog
+	// si no se llama igual, no se va a poder acceder a los posts desde la colección
+	// se pueden crear más colecciones, pero tienen que tener su propia carpeta
+	// y su propio esquema, por ejemplo:
+	// portfolio: defineCollection({
+	//     type: "content",
+	//     schema: z.object({
+	//         title: z.string(),
+	//         date: z.date(), y así
+
+	author: authorCollection,
 };
